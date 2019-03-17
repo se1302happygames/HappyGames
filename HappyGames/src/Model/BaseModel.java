ad42a34be154d15159aa7d10dc44c8c73361b563
annanguyen19 <33077255+annanguyen19@users.noreply.github.com>
2019-03-18 01:49:40 +0700

    Model/BaseModel.java

---
+++ HappyGames/src/Model/BaseModel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author anh
+ */
+public class BaseModel {
+   private int ID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
